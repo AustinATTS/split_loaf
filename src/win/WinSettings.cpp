cf0e103ae52c7b05d30de42d2de1430d9af3dbba
Austin Welsh-Graham <admin@toastysoftware.co.uk>
2025-12-05 00:06:04 +0000

    Made the settings kinda work, setting it to run on startup seems successful.

--- src/win/WinSettings.cpp
+++ src/win/WinSettings.cpp
@@ -9,10 +9,10 @@ static UINT lockKey   = VK_F9;
 static UINT unlockKey = VK_F10;
 
 static bool captureMode = false;
-static UINT * captureTarget = nullptr;
+static UINT* captureTarget = nullptr;
 
-static const char * RUN_KEY = "Software\\Microsoft\\Windows\\CurrentVersion\\Run";
-static const char * RUN_NAME = "SplitLoaf";
+static const char* RUN_KEY  = "Software\\Microsoft\\Windows\\CurrentVersion\\Run";
+static const char* RUN_NAME = "SplitLoaf";
 
 bool GetRunOnStartup ( ) {
     HKEY hKey;
@@ -23,13 +23,16 @@ bool GetRunOnStartup ( ) {
     char buffer[MAX_PATH];
     DWORD size = sizeof(buffer);
     bool exists = (RegQueryValueExA(hKey, RUN_NAME, NULL, NULL, (LPBYTE)buffer, & size) == ERROR_SUCCESS);
+
     RegCloseKey(hKey);
     return exists;
 }
 
 void SetRunOnStartup (bool enable) {
     HKEY hKey;
-    RegOpenKeyExA(HKEY_CURRENT_USER, RUN_KEY, 0, KEY_WRITE, & hKey);
+    if (RegOpenKeyExA(HKEY_CURRENT_USER, RUN_KEY, 0, KEY_WRITE, & hKey) != ERROR_SUCCESS) {
+        return;
+    }
 
     if (enable) {
         char exePath[MAX_PATH];
@@ -43,46 +46,69 @@ void SetRunOnStartup (bool enable) {
 }
 
 LRESULT CALLBACK SettingsWndProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+
     switch (msg) {
+
         case WM_CREATE: {
+            HINSTANCE hInst = (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE);
 
-            CreateWindow("STATIC", "Target Key:", WS_VISIBLE | WS_CHILD,20, 20, 150, 20, hwnd, NULL, NULL, NULL);
-            CreateWindow("BUTTON", "Change", WS_VISIBLE | WS_CHILD,200, 15, 100, 30, hwnd, (HMENU)1, NULL, NULL);
+            CreateWindowA("STATIC", "Target Key:", WS_VISIBLE | WS_CHILD,20, 20, 150, 20, hwnd, NULL, hInst, NULL);
 
-            CreateWindow("STATIC", "Lock Key:", WS_VISIBLE | WS_CHILD,20, 60, 150, 20, hwnd, NULL, NULL, NULL);
-            CreateWindow("BUTTON", "Change", WS_VISIBLE | WS_CHILD,200, 55, 100, 30, hwnd, (HMENU)2, NULL, NULL);
+            CreateWindowA("BUTTON", "Change", WS_VISIBLE | WS_CHILD,200, 15, 100, 30, hwnd, (HMENU)1, hInst, NULL);
 
-            CreateWindow("STATIC", "Unlock Key:", WS_VISIBLE | WS_CHILD,20, 100, 150, 20, hwnd, NULL, NULL, NULL);
-            CreateWindow("BUTTON", "Change", WS_VISIBLE | WS_CHILD,200, 95, 100, 30, hwnd, (HMENU)3, NULL, NULL);
+            CreateWindowA("STATIC", "Lock Key:", WS_VISIBLE | WS_CHILD,20, 60, 150, 20, hwnd, NULL, hInst, NULL);
 
-            CreateWindow("BUTTON", "Run on startup",WS_VISIBLE | WS_CHILD | BS_CHECKBOX,20, 140, 200, 20, hwnd, (HMENU)4, NULL, NULL);
+            CreateWindowA("BUTTON", "Change", WS_VISIBLE | WS_CHILD,200, 55, 100, 30, hwnd, (HMENU)2, hInst, NULL);
+
+            CreateWindowA("STATIC", "Unlock Key:", WS_VISIBLE | WS_CHILD,20, 100, 150, 20, hwnd, NULL, hInst, NULL);
+
+            CreateWindowA("BUTTON", "Change", WS_VISIBLE | WS_CHILD,200, 95, 100, 30, hwnd, (HMENU)3, hInst, NULL);
+
+            HWND chk = CreateWindowA("BUTTON", "Run on startup",WS_VISIBLE | WS_CHILD | BS_AUTOCHECKBOX,20, 140, 200, 20,hwnd, (HMENU)4, hInst, NULL);
 
             if (GetRunOnStartup()) {
-                SendDlgItemMessage(hwnd, 4, BM_SETCHECK, BST_CHECKED, 0);
+                SendMessage(chk, BM_SETCHECK, BST_CHECKED, 0);
             }
 
-            CreateWindow("BUTTON", "Close", WS_VISIBLE | WS_CHILD,120, 180, 100, 30, hwnd, (HMENU)10, NULL, NULL);
+            CreateWindowA("BUTTON", "Close", WS_VISIBLE | WS_CHILD,120, 180, 100, 30, hwnd, (HMENU)10, hInst, NULL);
         }
         break;
 
-        case WM_COMMAND:
+        case WM_COMMAND: {
             switch (LOWORD(wParam)) {
-                case 1: captureMode = true; captureTarget = & targetKey; break;
-                case 2: captureMode = true; captureTarget = & lockKey; break;
-                case 3: captureMode = true; captureTarget = & unlockKey; break;
+
+                case 1:
+                    captureMode = true;
+                    captureTarget = & targetKey;
+                    SetFocus(hwnd);
+                    break;
+
+                case 2:
+                    captureMode = true;
+                    captureTarget = & lockKey;
+                    SetFocus(hwnd);
+                    break;
+
+                case 3:
+                    captureMode = true;
+                    captureTarget = & unlockKey;
+                    SetFocus(hwnd);
+                    break;
 
                 case 4: {
-                    bool checked = SendDlgItemMessage(hwnd, 4, BM_GETCHECK, 0, 0) == BST_CHECKED;
-                    SetRunOnStartup(checked);
+                    bool isChecked = (SendDlgItemMessage(hwnd, 4, BM_GETCHECK, 0, 0) == BST_CHECKED);
+
+                    SetRunOnStartup(isChecked);
                 }
                 break;
 
                 case 10:
                     DestroyWindow(hwnd);
                     hWndSettings = NULL;
-                break;
+                    break;
             }
-            break;
+        }
+        break;
 
         case WM_KEYDOWN:
             if (captureMode && captureTarget) {
@@ -111,14 +137,14 @@ void OpenSettingsWindow (HINSTANCE hInstance, HWND parent) {
         return;
     }
 
-    WNDCLASS wc = {};
+    WNDCLASSA wc = {};
     wc.lpfnWndProc = SettingsWndProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = "SplitLoafSettings";
 
-    RegisterClass(& wc);
+    RegisterClassA(& wc);
 
-    hWndSettings = CreateWindow("SplitLoafSettings","Split Loaf Settings",WS_OVERLAPPEDWINDOW & ~WS_MAXIMIZEBOX & ~WS_THICKFRAME,CW_USEDEFAULT, CW_USEDEFAULT,360, 260,parent, NULL, hInstance, NULL);
+    hWndSettings = CreateWindowA("SplitLoafSettings","Split Loaf Settings",(WS_OVERLAPPEDWINDOW & ~WS_MAXIMIZEBOX & ~WS_THICKFRAME),CW_USEDEFAULT, CW_USEDEFAULT,360, 260,parent,NULL,hInstance,NULL);
 
     ShowWindow(hWndSettings, SW_SHOW);
 }
