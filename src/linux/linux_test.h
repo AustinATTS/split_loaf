7ac4e79b969f00aa4cfcbbc616909ef8fa9b94c2
Austin Welsh-Graham <admin@toastysoftware.co.uk>
2025-12-28 20:49:15 +0000

    Removed old files, I have a new plan which also likely wont work, but hey ho

--- src/linux/linux_test.h
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-#include <linux/uinput.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
-
-
-void emit (int fd, int type, int code, int val);
-int linux_main ( );
