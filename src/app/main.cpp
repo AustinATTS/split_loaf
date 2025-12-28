c250b235ee2bd36293d7e129bea5e2e3add0febf
Austin Welsh-Graham <admin@toastysoftware.co.uk>
2025-12-28 20:53:51 +0000

    Setup to be able to build the linux app

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,15 +1,16 @@
-#ifdef _WIN32 // Will only build the Windows version for Windows devices.
+#ifdef _WIN32
 #include <windows.h>
 extern int RunWindowsApp ( );
 
-// Windows expects a WinMain as the application entry point so it doesn't attempt to run it through the terminal.
 int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
-    return RunWindowsApp(); // Call the main start function from WinApp.cpp.
+    return RunWindowsApp();
 }
 
-#else // For other operating systems just return 0 as they haven't been implemented yet.
+#else
+
+extern int RunLinuxApp ( );
 
 int main ( ) {
-    return linux_main();
+    return RunLinuxApp();
 }
 #endif
